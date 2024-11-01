4de48f680ecd9c72b359ff2cd5fcfb52da36cee8
thiva-k <kalyanasundaramthivaharan@gmail.com>
2024-11-01 17:39:38 +0530

    Update pysqlite

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 import streamlit as st
 import os
 import sys
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 try:
     from chatbot import run_chatbot
